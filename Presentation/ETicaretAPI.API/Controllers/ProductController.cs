65b3054af5e6504428f479b3f4ea1675efdb5764
Furkan Erbay <furkanerbay41@hotmail.com>
2023-02-17 21:50:42 +0300

    Order Added

--- Presentation/ETicaretAPI.API/Controllers/ProductController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductController.cs
@@ -13,17 +13,21 @@ namespace ETicaretAPI.API.Controllers
         readonly private IProductReadRepository _productReadRepository;
 
         readonly private IOrderWriteRepository _orderWriteRepository;
+        readonly private IOrderReadRepository _orderReadRepository;
+
         readonly private ICustomerWriteRepository _customerWriteRepository;
 
         public ProductsController(IProductWriteRepository productWriteRepository,
             IProductReadRepository productReadRepository,
             IOrderWriteRepository orderWriteRepository,
-            ICustomerWriteRepository customerWriteRepository)
+            ICustomerWriteRepository customerWriteRepository,
+            IOrderReadRepository orderReadRepository)
         {
             _productWriteRepository = productWriteRepository;
             _productReadRepository = productReadRepository;
             _orderWriteRepository = orderWriteRepository;
             _customerWriteRepository = customerWriteRepository;
+            _orderReadRepository = orderReadRepository;
         }
         [HttpGet()]
         public async Task Get()
@@ -35,9 +39,9 @@ namespace ETicaretAPI.API.Controllers
             //});
             //await _productWriteRepository.SaveAsync();
 
-            await _productWriteRepository.AddAsync(new() { Name = "C Product", Price = 1.500F, Stock = 10 });
+            //await _productWriteRepository.AddAsync(new() { Name = "C Product", Price = 1.500F, Stock = 10 });
 
-            await _productWriteRepository.SaveAsync();
+            //await _productWriteRepository.SaveAsync();
 
             //var customerId = Guid.NewGuid();
             //await _customerWriteRepository.AddAsync(new() { Id = customerId, Name = "Muhittin" });
@@ -45,6 +49,11 @@ namespace ETicaretAPI.API.Controllers
             //await _orderWriteRepository.AddAsync(new() { Description = "bla bla bla", Address = "Ankara/Çankaya",CustomerId = customerId });
             //await _orderWriteRepository.AddAsync(new() { Description = "bla bla bla 2", Address = "Ankara/Pursaklar" , CustomerId = customerId});
             //await _orderWriteRepository.SaveAsync();
+
+
+            Order order = await _orderReadRepository.GetByIdAsync("75070da5-c343-46a4-83b3-2baefb43a4f0");
+            order.Address = "Istanbul";
+            await _orderWriteRepository.SaveAsync();
         }
 
         
