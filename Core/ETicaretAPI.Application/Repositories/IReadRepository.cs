743bcb27a309b261327458104a9af56656eb2909
furka <furka@DESKTOP-BRK73O0>
2023-02-17 03:40:20 +0300

    Add project files.

---
+++ Core/ETicaretAPI.Application/Repositories/IReadRepository.cs
@@ -0,0 +1,20 @@
+﻿using ETicaretAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Repositories
+{
+    public interface IReadRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        //IQueryable Sorgu üzerinde çalışmak için
+        //IENumarable : Memory de
+        IQueryable<T> GetAll();
+        IQueryable GetWhere(Expression<Func<T,bool>> method);
+        Task<T> GetSingleAsync(Expression<Func<T,bool>> method);
+        Task<T> GetByIdAsync(Guid id);
+    }
+}
---
+++ Infrastructure/ETicaretAPI.Persistence/ServiceRegistiration.cs
@@ -0,0 +1,30 @@
+﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Persistence.Contexts;
+using ETicaretAPI.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence
+{
+    public static class ServiceRegistiration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddDbContext<ETicaretAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString),ServiceLifetime.Singleton);
+            
+            services.AddSingleton<ICustomerReadRepository,CustomerReadRepository>();
+            services.AddSingleton<ICustomerWriteRepository, CustomerWriteRepository>();
+
+            services.AddSingleton<IOrderWriteRepository, OrderWriteRepository>();
+            services.AddSingleton<IOrderReadRepository, OrderReadRepository>();
+
+            services.AddSingleton<IProductReadRepository, ProductReadRepository>();
+            services.AddSingleton<IProductWriteRepository, ProductWriteRepository>();
+        }
+    }
+}
